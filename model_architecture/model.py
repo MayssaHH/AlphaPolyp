e3a9a5689819fddd4db86263e68e0a0f0fac3c53
Mayssa Hajj Hassan <mayssahajj03@gmail.com>
2025-07-05 16:04:29 +0300

    Add print statement

--- model_architecture/model.py
+++ model_architecture/model.py
@@ -28,41 +28,45 @@ def create_model(img_height, img_width, input_channels, out_classes, starting_fi
     print("output shape of p1", p1.shape)
     #from metaformer
     p2 = Conv2D(starting_filters*4,1,padding='same')(layers[3]) 
+    print("output shape of p2", p2.shape)   
     p3 = Conv2D(starting_filters*8,1,padding='same')(layers[2]) 
+    print("output shape of p3", p3.shape)
     p4 = Conv2D(starting_filters*16,1,padding='same')(layers[1])     
+    print("output shape of p4", p4.shape)
     p5 = Conv2D(starting_filters*32,1,padding='same')(layers[0]) 
+    print("output shape of p5", p5.shape)
     
     
     t0 = RAPU(input_layer, starting_filters)  
-
+    print("output shape of t0", t0.shape)
     # Encoder: RAPU + downsampling
     l1i = Conv2D(starting_filters * 2, 2, strides=2, padding='same')(t0)    
     s1 = add([l1i, p1])     
     t1 = RAPU(s1, starting_filters * 2)       
-   
+    print("output shape of t1", t1.shape)
     
     l2i = Conv2D(starting_filters * 4, 2, strides=2, padding='same')(t1)
     s2 = add([l2i, p2])
     t2 = RAPU(s2, starting_filters * 4) 
-    
+    print("output shape of t2", t2.shape)
 
     l3i = Conv2D(starting_filters * 8, 2, strides=2, padding='same')(t2)
     s3 = add([l3i, p3])
     t3 = RAPU(s3, starting_filters * 8) 
-   
+    print("output shape of t3", t3.shape)
     l4i = Conv2D(starting_filters * 16, 2, strides=2, padding='same')(t3)
     s4 = add([l4i, p4])
     t4 = RAPU(s4, starting_filters * 16)
-    
+    print("output shape of t4", t4.shape)
     l5i = Conv2D(starting_filters * 32, 2, strides=2, padding='same')(t4)
     s5 = add([l5i, p5]) 
-    
+    print("output shape of s5", s5.shape)
     # bottleneck: ResNet Blocks 
     t51 = resnet_block(s5, starting_filters*32)
     t51 = resnet_block(t51, starting_filters*32) 
     t53 = resnet_block(t51, starting_filters*16) 
     t53 = resnet_block(t53, starting_filters*16) 
-
+    print("output shape of t53", t53.shape)
     # Save this "t53" as final encoder output
     encoder_output = t53
         
@@ -73,28 +77,33 @@ def create_model(img_height, img_width, input_channels, out_classes, starting_fi
     outd = tf.keras.layers.Concatenate(axis=-1)([outd, t3]) 
     outd = convf_bn_act(outd, dim, 1)
     outd = Conv2D(1, kernel_size=1, use_bias=False)(outd)  
-    
+    print("output shape of outd", outd.shape)
     L_input = convf_bn_act(t2,dim, 3)   
-    
+    print("output shape of L_input", L_input.shape)
     H_input = tf.keras.layers.Concatenate(axis=-1)([UpSampling2D((2,2))(t53), t4])
     H_input = convf_bn_act(H_input, dim,1)
     H_input = UpSampling2D((2,2))(H_input) 
-    
+    print("output shape of H_input", H_input.shape)
     out2 = SBA(L_input, H_input)   
-         
+    print("output shape of out2", out2.shape)
     out1 = UpSampling2D(size=(8,8), interpolation='bilinear')(outd)
     out2 = UpSampling2D(size=(4,4), interpolation='bilinear')(out2)
-        
+    print("output shape of out1", out1.shape)
+    print("output shape of out2", out2.shape)
     out_duat = out1+out2 
+    print("output shape of out_duat", out_duat.shape)
     segmentation_output = Conv2D(out_classes, (1, 1), activation='sigmoid')(out_duat)
-
+    print("output shape of segmentation_output", segmentation_output.shape)
     # Regression Model
     seg_down = AveragePooling2D(pool_size=(32,32))(segmentation_output)
     combined = tf.keras.layers.Concatenate(axis=-1)([encoder_output, seg_down])
-
+    print("output shape of combined", combined.shape)
     x = GlobalAveragePooling2D()(combined)
+    print("output shape of x", x.shape)
     x = tf.keras.layers.Dense(64, activation='relu')(x)
+    print("output shape of x", x.shape)
     x = tf.keras.layers.Dense(32, activation='relu')(x)
+    print("output shape of x", x.shape)
 
     # Initialize the bias of the regression output layer
     bias_init = Constant(reg_mean_norm) if reg_mean_norm is not None else "zeros"
